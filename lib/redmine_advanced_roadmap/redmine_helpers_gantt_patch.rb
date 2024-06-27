4d4729968f13c02bbbfaff6b784bf407432015fa
Moritz Gschwandtner <gschwandtner@artech.at>
2024-06-27 13:02:57 +0200

    fix(redmine 5): fix gantt crash

--- lib/redmine_advanced_roadmap/redmine_helpers_gantt_patch.rb
+++ lib/redmine_advanced_roadmap/redmine_helpers_gantt_patch.rb
@@ -7,7 +7,7 @@ module RedmineAdvancedRoadmap
 
         def render_version_with_milestones(project, version, options = {})
           if @last_rendered_project != project and project.milestones.any?
-            subject_for_milestones_label(options)
+            subject_for_milestones_label(options, project)
             options[:top] += options[:top_increment]
             @number_of_rows += 1
             options[:indent] += options[:indent_increment]
@@ -24,19 +24,23 @@ module RedmineAdvancedRoadmap
 
         def render_milestone(project, milestone, options = {})
           # Milestone header
-          subject_for_milestone(milestone, options) unless options[:only] == :lines
+          subject_for_milestone(milestone, options, project) unless options[:only] == :lines
           line_for_milestone(milestone, options) unless options[:only] == :subjects
           options[:top] += options[:top_increment]
           @number_of_rows += 1
         end
 
-        def subject_for_milestones_label(options)
+        def subject_for_milestones_label(options, prj)
           case options[:format]
           when :html
             html_class = 'icon icon-milestones'
             s = l(:label_milestone_plural)
             subject = view.content_tag(:span, s, :class => html_class).html_safe
-            html_subject(options, subject, :css => "milestones-label")
+            if Redmine::VERSION::MAJOR == 5
+              html_subject(options, subject, nil)
+            else
+              html_subject(options, subject, :css => "milestones-label")
+            end
           when :image
             image_subject(options, l(:label_milestone_plural))
           when :pdf
@@ -45,13 +49,17 @@ module RedmineAdvancedRoadmap
           end
         end
 
-        def subject_for_milestone(milestone, options)
+        def subject_for_milestone(milestone, options, prj)
           case options[:format]
           when :html
             html_class = 'icon icon-milestone'
             s = view.link_to_milestone(milestone).html_safe
             subject = view.content_tag(:span, s, :class => html_class).html_safe
-            html_subject(options, subject, :css => "milestone-name")
+            if Redmine::VERSION::MAJOR == 5
+              html_subject(options, subject, milestone)
+            else
+              html_subject(options, subject, :css => "milestone-name")
+            end
           when :image
             image_subject(options, milestone.to_s)
           when :pdf
@@ -71,18 +79,24 @@ module RedmineAdvancedRoadmap
             when :html
               if Redmine::VERSION::MAJOR == 2
                 html_task(options, coords, :css => "version task", :label => label, :markers => true)
+              elsif Redmine::VERSION::MAJOR == 5
+                html_task(options, coords, true, label, milestone)
               else
                 html_task(options, coords, true, label, Version)
               end
             when :image
               if Redmine::VERSION::MAJOR == 2
                 image_task(options, coords, :label => label, :markers => true, :height => 3)
+              elsif Redmine::VERSION::MAJOR == 5
+                image_task(options, coords, true, label, milestone)
               else
                 image_task(options, coords, true, label, Version)
               end
             when :pdf
               if Redmine::VERSION::MAJOR == 2
                 pdf_task(options, coords, :label => label, :markers => true, :height => 0.8)
+              elsif Redmine::VERSION::MAJOR == 5
+                pdf_task(options, coords, true, label, milestone)
               else
                 pdf_task(options, coords, true, label, Version)
               end
